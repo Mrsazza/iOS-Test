b36942bf3f5c1e56b1373239d6191887819e9521
Mrsazza <shahriarhossain147@gmail.com>
2022-05-31 17:36:31 +0600

    Initial Commit

---
+++ iOS Test/iOS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TestApp.swift
+//  iOS Test
+//
+//  Created by Sazza on 31/5/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
